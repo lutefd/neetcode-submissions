827a69ac04de69debb0bf02aee5e035c51323625
Luis Dourado <101512529+lutefd@users.noreply.github.com>
2026-09-06 15:03:46 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "false"
+print(type(variable))
+variable = []
+print(type(variable))
